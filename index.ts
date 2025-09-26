67295778e775d1ff1474904e5f5e753eb88c7323
shanyan03 <136148297+shanyan03@users.noreply.github.com>
2025-09-26 09:47:16 +0800

    Delete index.ts

--- index.ts
+++
@@ -1,7 +0,0 @@
-// index.ts
-import "react-native-gesture-handler";
-import { registerRootComponent } from "expo";
-import App from "./App";
-
-// This tells RN/Expo what the "main" app is
-registerRootComponent(App);
